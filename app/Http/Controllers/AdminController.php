7385e6efedc5488c1d71dd6cd45ca17b4baf0b2e
Eduardo <chessed03@gmail.com>
2022-08-08 17:16:05 -0500

    Guzzle Client

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Psr7\Request as ClientRequest;
+use \GuzzleHttp\Client as ClientHTTP;
 use Illuminate\Http\Request;
+use GuzzleHttp\Psr7\Utils;
 use GuzzleHttp\Psr7;
 
 class AdminController extends Controller
@@ -18,7 +20,13 @@ class AdminController extends Controller
     public function sendForm(Request $request)
     {
         
-        $client = new \GuzzleHttp\Client(); 
+        $client        = new ClientHTTP(); // or \GuzzleHttp\Client();
+
+        $api_url       = 'http://127.0.0.1:8000/api/processDataFile';
+
+        $location_file = '/home/eduardo/Documentos/00_archivos_test/emails to process.txt';
+
+        $source_file   = Utils::tryFopen( $location_file, 'r');
 
         $headers = [
             'Accept' => 'application/json',
@@ -29,8 +37,8 @@ class AdminController extends Controller
             'multipart' => [
                     [
                     'name' => 'file',
-                    'contents' => Psr7\Utils::tryFopen('/home/eduardo/Documentos/00_archivos_test/emails to process.txt', 'r'),
-                    'filename' => '/home/eduardo/Documentos/00_archivos_test/emails to process.txt',
+                    'contents' => $source_file,
+                    'filename' => $location_file,
                     'headers'  => [
                         'Content-Type' => '<Content-type header>'
                         ]
@@ -38,7 +46,8 @@ class AdminController extends Controller
                 ]
         ];
         
-        $request = new Psr7\Request('POST', 'http://127.0.0.1:8000/api/processDataFile', $headers);
+        $request = new ClientRequest('POST', $api_url, $headers);
+
         $res = $client->sendAsync($request, $options)->wait();
 
     
